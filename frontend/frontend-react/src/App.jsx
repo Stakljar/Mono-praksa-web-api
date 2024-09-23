6fcbfbb8feb1b0ff31a09e5e05f9e5d4930ff3ef
Dražen Antunović <drazenantunovic012@gmail.com>
2024-09-23 13:47:24 +0200

    Added react implementation

---
+++ frontend/frontend-react/src/App.jsx
@@ -0,0 +1,53 @@
+import './App.css';
+import CatTable from './components/CatTable';
+import AddCatForm from './components/AddCatForm';
+import UpdateCatModal from './components/UpdateCatModal';
+import { useState } from 'react';
+
+function App() {
+  const [cats, setCats] = useState([
+    { id: 1, name: 'Havoc', age: 3, color: 'black' },
+    { id: 2, name: 'Zelg', age: 1, color: 'black' },
+  ]);
+
+  const [catToEdit, setCatToEdit] = useState(null);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AddCatForm addCat={(newCat) => {
+          setCats([...cats, newCat]);
+        }} />
+
+        <h1 className="cat-table-title">Cats</h1>
+        <CatTable cats={cats}
+          editCat={(cat) => {
+            setCatToEdit(cat);
+          }}
+          deleteCat={(cat) => {
+            if (window.confirm("Are you sure you want to delete this cat from the list?")) {
+              setCats((prevCats) => prevCats.filter(c => c.id !== cat.id));
+            }
+          }}
+        />
+
+        {
+          catToEdit ?
+            <UpdateCatModal
+              isOpen={catToEdit}
+              catToEdit={catToEdit}
+              onClose={() => {
+                setCatToEdit(null);
+              }}
+              updateCat={(updatedCat) => {
+                setCats(cats.map((cat) => (cat.id === updatedCat.id ? updatedCat : cat)));
+              }}
+            /> : ""
+        }
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/frontend-react/src/components/AddCatForm.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+export default function AddCatForm({ addCat }) {
+    const [cat, setCat] = useState({name: "", age: "", color: ""})
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addCat(cat);
+        setCat({name: "", age: "", color: ""});
+    };
+
+    const handleChange = (e) => {
+        setCat((prev) => { return { ...prev, [e.target.name]: e.target.value }})
+    }
+
+    return (
+        <form id="catForm" onSubmit={handleSubmit}>
+            <label htmlFor="catName">Cat's Name:</label><br />
+            <input
+                type="text"
+                id="catName"
+                name="name"
+                value={cat.name}
+                onChange={handleChange}
+                placeholder="Enter cat's name"
+                required
+            /><br /><br />
+
+            <label htmlFor="catAge">Cat's Age:</label><br />
+            <input
+                type="number"
+                id="catAge"
+                name="age"
+                value={cat.age}
+                onChange={handleChange}
+                min="0"
+                max="35"
+                placeholder="Enter cat's age"
+                required
+            /><br /><br />
+
+            <label htmlFor="catColor">Cat's Color:</label><br />
+            <input
+                type="text"
+                id="catColor"
+                name="color"
+                value={cat.color}
+                onChange={handleChange}
+                placeholder="Enter cat's color"
+                required
+            /><br /><br />
+
+            <input type="submit" value="Add Cat" />
+        </form>
+    );
+}
